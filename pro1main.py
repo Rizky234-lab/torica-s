fad536eddb4c64edf39a77ac6f4740c20c28dc3d
TonyPutraS <85999562+tonyputrasantoso@users.noreply.github.com>
2024-09-26 10:40:10 +0700

    Update pro1main.py

--- pro1main.py
+++ pro1main.py
@@ -1,36 +1,29 @@
-# line untuk meminta input file dari pengguna
-def request_file():
-    while True:
-        filename = input("Masukkan nama file (dengan txt atau csv): ")
-        if os.path.exists(filename):
-            return filename
-        else:
-            print(f"File {filename} tidak ditemukan. coba lagi pak.")
+from pel import baca_file, parse_array, parse_dictionary
 
-# fungsi utama
 def main():
-    print("Aplikasi Pembaca File: Array/Dictionary")
-
-    # meminta nama file dari pengguna
-    filename = request_file()
-    # meminta pengguna untuk memilih format file (array atau dictionary)
     while True:
-        file_format = input("Masukkan format file (array/dictionary): ").strip().lower()
-
-        if file_format == 'array':
-            array_data = read_array_file(filename)
-            print("Isi file dalam format Array:")
-            print(array_data)
-            break
-        elif file_format == 'dictionary':
-            dict_data = read_dict_file(filename)
-            print("Isi file dalam format Dictionary:")
-            for key, value in dict_data.items():
-                print(f"{key}: {value}")
-            break
+        nama_file = input("Masukkan nama file txt/csv: ")
+        isi_file = baca_file(nama_file)
+        if isi_file is not None:
+            if '=>' in isi_file or ':' in isi_file:
+                #ini Dictionary
+                data_dict = parse_dictionary(isi_file)
+                print("\nData yang dibaca adalah Dictionary:")
+                print(data_dict)
+            else:
+                #ini Array
+                data_array = parse_array(isi_file)
+                print("\nData yang dibaca adalah Array:")
+                print(data_array)
         else:
-            print("Format tidak dikenali. Silakan masukkan 'array' atau 'dictionary'.")
+            print(f"File '{nama_file}' tidak ditemukan.coba lagi.\n")
+        
+        #tanyakan masukkan lagi / tidak
+        pilihan = input("\nApakah Anda ingin memasukkan file lain? (y/n): ").lower()
+        if pilihan != 'y':
+            print("Terima kasih telah menggunakan aplikasi!")
+            break
 
-# menjalankan program
+
 if _name_ == "_main_":
     main()
