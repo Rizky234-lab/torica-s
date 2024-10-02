d7dfe8840dccbebe991d67ac64e81853866fe978
cas <amandakylac@gmail.com>
2024-10-03 05:56:22 +0700

    Update apkfurniture.py

--- apkfurniture.py
+++ apkfurniture.py
@@ -22,3 +22,37 @@ class FurnitureManager:
                 return furniture_list
         except FileNotFoundError:
             return []
+
+
+def tambah_data(self, nama_barang, warna, ukuran):
+        new_furniture = Furniture(nama_barang, warna, ukuran)
+        self.data.append(new_furniture)
+        self.tulis_data()
+
+    def hapus_data(self, nama_barang):
+        for furniture in self.data:
+            if furniture.nama_barang == nama_barang:
+                self.data.remove(furniture)
+                self.tulis_data()
+                print(f"Data {nama_barang} berhasil dihapus.")
+                return
+        print(f"Data {nama_barang} tidak ditemukan.")
+
+    def edit_data(self, nama_barang, warna=None, ukuran=None):
+        for furniture in self.data:
+            if furniture.nama_barang == nama_barang:
+                if warna:
+                    furniture.warna = warna
+                if ukuran:
+                    furniture.ukuran = ukuran
+                self.tulis_data()
+                print(f"Data {nama_barang} berhasil diedit.")
+                return
+        print(f"Data {nama_barang} tidak ditemukan.")
+
+    def tulis_data(self):
+        with open(self.file_name, 'w') as file:
+            file.write("Nama Barang,Warna,Ukuran\n")
+            for furniture in self.data:
+                file.write(f"{furniture.nama_barang},{furniture.warna},{furniture.ukuran}\n")
+
