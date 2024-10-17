7296e30f1b8747c40e7dfa7d5d2f6159b9a5573e
TonyPutraS <85999562+tonyputrasantoso@users.noreply.github.com>
2024-10-17 09:50:23 +0700

    Update gui1.py

--- gui1.py
+++ gui1.py
@@ -99,3 +99,102 @@ class Application(tk.Tk):
         # ukuran tambah frame
         label_tambah = tk.Label(self.ukuran_tambah_frame, text="Tambah Ukuran Baru", font=("Helvetica", 12))
         label_tambah.pack(pady=5)
+
+
+        self.ukuran_entry = tk.Entry(self.ukuran_tambah_frame, width=30)
+        self.ukuran_entry.pack(pady=5)
+        
+        tambah_button = tk.Button(self.ukuran_tambah_frame, text="Tambah Ukuran", command=self.tambah_ukuran)
+        tambah_button.pack(pady=10)
+        
+        back_button = tk.Button(self.ukuran_tambah_frame, text="BACK", command=lambda: self.show_data('ukuran'))
+        back_button.pack(pady=10)
+        
+        # furniture tambah frame
+        label_tambah = tk.Label(self.furniture_tambah_frame, text="Tambah furniture Baru", font=("Helvetica", 12))
+        label_tambah.pack(pady=5)
+        
+        self.nama_furniture_entry = tk.Entry(self.furniture_tambah_frame, width=30)
+        self.nama_furniture_entry.pack(pady=5)
+
+        self.ukuran_var = tk.StringVar()
+        self.warna_var = tk.StringVar()
+        
+        label_ukuran = tk.Label(self.furniture_tambah_frame, text="Pilih Ukuran", font=("Helvetica", 10))
+        label_ukuran.pack(pady=5)
+        
+        self.ukuran_option = tk.OptionMenu(self.furniture_tambah_frame, self.ukuran_var, *self.data_manager.list_data('ukuran').keys())
+        self.ukuran_option.pack(pady=5)
+        
+        label_warna = tk.Label(self.furniture_tambah_frame, text="Pilih Warna", font=("Helvetica", 10))
+        label_warna.pack(pady=5)
+        
+        self.warna_option = tk.OptionMenu(self.furniture_tambah_frame, self.warna_var, *self.data_manager.list_data('warna').keys())
+        self.warna_option.pack(pady=5)
+        
+        tambah_button = tk.Button(self.furniture_tambah_frame, text="Tambah Furniture", command=self.tambah_furniture)
+        tambah_button.pack(pady=10)
+        
+        back_button = tk.Button(self.furniture_tambah_frame, text="BACK", command=lambda: self.show_data('furniture'))
+        back_button.pack(pady=10)
+
+        # Detail frame
+        self.detail_label = tk.Label(self.detail_frame, text="", font=("Helvetica", 12))
+        self.detail_label.pack(pady=10)
+
+        back_button = tk.Button(self.detail_frame, text="BACK", command=self.show_data)
+        back_button.pack(pady=10)
+
+        self.edit_frame = tk.Frame(self)
+        label_edit = tk.Label(self.edit_frame, text="Edit Data", font=("Helvetica", 12))
+        label_edit.pack(pady=5)
+        
+        self.edit_entry = tk.Entry(self.edit_frame, width=30)
+        self.edit_entry.pack(pady=5)
+        
+        save_button = tk.Button(self.edit_frame, text="SAVE", command=self.save_edit)
+        save_button.pack(pady=10)
+        
+        back_button = tk.Button(self.edit_frame, text="BACK", command=self.show_data)
+        back_button.pack(pady=10)
+
+    def show_home(self):
+        self.hide_all_frames()
+        self.home_frame.pack()
+
+    def show_data(self, data_type=None):
+        self.hide_all_frames()
+        if data_type:
+            self.current_data_type = data_type
+        if self.current_data_type == 'furniture':
+            frame = self.furniture_frame
+        elif self.current_data_type == 'warna':
+            frame = self.warna_frame
+        elif self.current_data_type == 'ukuran':
+            frame = self.ukuran_frame
+        frame.pack()
+        self.update_listbox(self.current_data_type)
+
+    def show_tambah_warna(self):
+        self.hide_all_frames()
+        self.warna_tambah_frame.pack()
+        
+    def show_tambah_ukuran(self):
+        self.hide_all_frames()
+        self.ukuran_tambah_frame.pack()
+        
+    def show_tambah_furniture(self):
+        self.hide_all_frames()
+        self.furniture_tambah_frame.pack()
+        self.update_options()
+
+    def hide_all_frames(self):
+        for frame in (self.home_frame, self.furniture_frame, self.warna_frame, self.ukuran_frame, self.warna_tambah_frame, self.ukuran_tambah_frame, self.furniture_tambah_frame, self.detail_frame, self.edit_frame):
+            frame.pack_forget()
+
+    def update_listbox(self, data_type):
+        listbox = self.furniture_frame.winfo_children()[1] if data_type == 'furniture' else self.warna_frame.winfo_children()[1] if data_type == 'warna' else self.ukuran_frame.winfo_children()[1]
+        listbox.delete(0, tk.END)
+        items = self.data_manager.list_data(data_type)
+        for item_id, item_name in items.items():
+            listbox.insert(tk.END, f"{item_id}: {item_name}")
